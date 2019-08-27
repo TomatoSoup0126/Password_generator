7ffc93855c7867c542efb9898b5fc3cbdc24a6f4
jonathantang <jonathantang0126@gmail.com>
2019-08-27 23:01:54 +0800

    modify UX

--- generate_password.js
+++ generate_password.js
@@ -50,6 +50,11 @@ function generatePassword(options) {
     )
   }
 
+  //return error notice if collection is empty
+  if (collection.length === 0) {
+    return 'There is no valid characters in your selection'
+  }
+
 
   // start generating password
   let password = ''
