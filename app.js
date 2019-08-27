e912c6376d8e9f411eae05300eb866cd6d83e527
jonathantang <jonathantang0126@gmail.com>
2019-08-27 15:42:20 +0800

    setting post method

--- app.js
+++ app.js
@@ -1,6 +1,9 @@
 //includ package and define server related variable
 const express = require('express')
 const exphbs = require('express-handlebars')
+
+const bodyParser = require('body-parser')
+
 const app = express()
 const port = 3000
 
@@ -8,10 +11,20 @@ const port = 3000
 app.engine('handlebars', exphbs({ defaultLayout: 'main' }))
 app.set('view engine', 'handlebars')
 
+//setting body-parser
+app.use(bodyParser.urlencoded({ extended: true }))
+
 //setting routes
 app.get('/', (req, res) => {
   res.render('index')
 })
+
+app.post('/', (req, res) => {
+  console.log('get form POST request')
+  console.log('req.body', req.body)
+  res.render('index')
+})
+
 
 //starts the express server and listening for connection.
 app.listen(port, () => {
